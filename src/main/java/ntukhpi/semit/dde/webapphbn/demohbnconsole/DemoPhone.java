d1eac013b3b1202b0f522c706aae644ce3ec10db
ddimae72 <ddimae72@gmail.com>
2023-04-13 16:55:50 +0300

    Add in DemoPhones blocks to save in DB and show phones by owner

--- src/main/java/ntukhpi/semit/dde/webapphbn/demohbnconsole/DemoPhone.java
+++ src/main/java/ntukhpi/semit/dde/webapphbn/demohbnconsole/DemoPhone.java
@@ -1,55 +1,83 @@
 package ntukhpi.semit.dde.webapphbn.demohbnconsole;
 
-import ntukhpi.semit.dde.webapphbn.dbconnect.DBConnect;
+import ntukhpi.semit.dde.webapphbn.doaccess.DAOEmployeesHBN;
 import ntukhpi.semit.dde.webapphbn.entities.Employee;
+import ntukhpi.semit.dde.webapphbn.entities.INN;
 import ntukhpi.semit.dde.webapphbn.entities.Phone;
 import ntukhpi.semit.dde.webapphbn.entities.PhoneNumberType;
 import ntukhpi.semit.dde.webapphbn.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.criterion.Restrictions;
+import org.hibernate.query.Query;
 
-import java.sql.Connection;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 public class DemoPhone {
     public static void main(String[] args) {
-        Connection con = DBConnect.getConnectionMySQL();
-        if (con == null) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        String phoneNumber = "";
+        String newPhoneNumber = "";
+        List<Phone> results = null;
+        String nameForInsert = "";
+        Employee owner = null;
+        Phone phoneFromDB = null;
+        if (sf == null) {
             System.err.println("Connect to database not executed!!!");
-            System.exit(999);
+            System.exit(777);
         } else {
             Transaction transaction = null;
-            try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            try (Session session = sf.openSession()) {
                 transaction = session.beginTransaction();
-//                Employee owner = DAOEmployeesHBN.getEmployeeById(3);
-                String nameForInsert = "Kovrova";
-                Employee owner = new Employee(-1l, nameForInsert, true, 35, 150000.0);
+                //Add new Employee
+                nameForInsert = "Lozhkin";
+                owner = new Employee(-1l, nameForInsert, false, 33, 77000.0);
                 System.out.println(owner);
-                session.save(owner);
-//                owner = session.get(Employee.class, owner.getId());
-                try {
-                    owner = (Employee) session.createCriteria(Employee.class)
-                            .add(Restrictions.like("name", nameForInsert))
-                            .list().get(0);
-                    System.out.println("Inn will be added to owner =" + owner.toString());
-                } catch (Exception ex) {
-                    System.out.println("******* No owner in db ********");
-                   // owner = new Employee(-1l, nameForInsert, true, 35, 50000.0);
-                    owner = new Employee();
+                if (DAOEmployeesHBN.insert(owner)) {
+                    System.out.println("\nNew Employee added");
                 }
+                owner = DAOEmployeesHBN.getEmployeeByName(nameForInsert);
+                //Add Phones to new Employee
+
+                System.out.println("\nGo add phones!");
                 Phone modile = new Phone(-1l, "0951203066", PhoneNumberType.MOBILE, true, true,owner);
                 System.out.println(modile.toString());
                 session.save(modile);
                 Phone home = new Phone(-1l, "0573153686", PhoneNumberType.HOME, true, false,owner);
                 System.out.println(home.toString());
                 session.save(home);
+                Phone office = new Phone(-1l, "0677573344", PhoneNumberType.OFFICE, true, false,owner);
+                System.out.println(home.toString());
+                session.save(office);
                 transaction.commit();
             } catch (Exception ex) {
-                System.err.println("===== save Inn == Something went wrong! ====");
+                System.err.println("===== save Phone == Something went wrong! ====");
                 if (transaction != null) {
                     transaction.rollback();
                 }
-
+            }
+            //Read phones by owner
+            //Find in DB by owner
+            try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+                owner = DAOEmployeesHBN.getEmployeeByName(nameForInsert);
+                System.out.println("\nPhones for owner "+owner);
+                CriteriaBuilder cb = session.getCriteriaBuilder();
+                CriteriaQuery<Phone> cr = cb.createQuery(Phone.class);
+                Root<Phone> root = cr.from(Phone.class);
+                cr.select(root).where(cb.equal(root.get("owner"), owner));
+                Query<Phone> query = session.createQuery(cr);
+                results = query.getResultList();
+                if (!results.isEmpty()) {
+                    System.out.println("Pones list");
+                    results.stream().forEach(System.out::println);
+                } else {
+                    System.out.println("Pones ABSENT!");
+                }
+            } catch (Exception e) {
+                System.err.println("******* Find INN in db (ByNumber) PROBLeM ********");
             }
 
         }
--- src/main/java/ntukhpi/semit/dde/webapphbn/entities/Phone.java
+++ src/main/java/ntukhpi/semit/dde/webapphbn/entities/Phone.java
@@ -21,15 +21,14 @@ public class Phone {
     @Column(name="phone_number", nullable=false, length=10,unique = true)
     private String phoneNumber;
     //https://www.javaguides.net/2018/11/hibernate-5-enum-type-mapping-example.html
-    @Enumerated(EnumType.STRING)
 
-    // How to set values by default
-    // https://ask-dev.ru/info/36428/setting-default-values-for-columns-in-jpa
+    @Enumerated(EnumType.STRING)
     //HOME, MOBILE, OFFICE
-    //@Enumerated(EnumType.STRING)
     @Column(name = "number_type", nullable = false, length = 10)
     @Convert(converter = PhoneNumberTypeConverter.class)
     //@ColumnDefault(value="MOBILE")
+    // https://ask-dev.ru/info/36428/setting-default-values-for-columns-in-jpa
+    // How to set values by default ?!
     private PhoneNumberType phoneNumberType;
 
     @Column(name = "active",nullable = false)
@@ -44,16 +43,9 @@ public class Phone {
     //https://www.javaguides.net/2022/02/spring-data-jpa-one-to-many-bidirectional-mapping.html
     // default fetch type for ManyToOne: EAGER
     @ManyToOne(fetch = FetchType.LAZY)
+    //updatable = false <==== Not take part in UPDATE ====> you can not change owner of this number. only delete
     @JoinColumn(name = "employee_id",nullable = false,updatable = false)
     private Employee owner;
-
-    public Employee getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Employee owner) {
-        this.owner = owner;
-    }
 
 
 }
